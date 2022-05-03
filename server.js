8cca2bb00a6c3213b216272f75ad181ca08714bd
asketsystem <asketsystem1@gmail.com>
2022-05-03 07:24:58 +0000

    Adding get,post,path,delete

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const Task = require("./models/tasks.model");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI =
@@ -9,14 +9,56 @@ const MONGODB_URI =
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// GET http://localhost:5000/getTask
+app.get("/getTasks", async (req, res) => {
+  try {
+    const response = await Task.find();
+    res.json(response);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// POST http://localhost:5000/postTask
+
+app.post("/postTask", async (req, res) => {
+  try {
+    const response = await Task.create(req.body);
+    res.json(response);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// DELETE http://localhost:5000/deleteTask/:id
+
+app.delete("/deleteTask/:id", async (req, res) => {
+  try {
+    const response = await Task.remove({ _id: req.params.id });
+    res.json(response);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+// PATCH http://localhost:5000/editTask/:id
+
+app.patch("/editTask/:id", async (req, res) => {
+  try {
+    const response = await Task.updateOne(
+      { _id: req.params.id },
+      { $set: req.body }
+    );
+    res.json(response);
+  } catch (err) {
+    res.json({ message: err });
+  }
 });
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-    app.listen(PORT, console.log("Server stated on port 5000"));
+    app.listen(PORT, console.log("Server started on port 5000"));
   })
   .catch((err) => {
     console.log(err);
